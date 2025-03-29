c81e85f999b1193d524ca3d0567fffde5e6da8c9
Zee <81491956+Zatom1@users.noreply.github.com>
2025-03-29 01:11:57 -0500

    Update Dynamic-MP-Sim.java

--- Dynamic-MP-Sim.java
+++ Dynamic-MP-Sim.java
@@ -1,19 +1,5 @@
-/**
- * Bounce. 
- * 
- * When the shape hits the edge of the window, it reverses its direction. 
- */
+
 import java.util.ArrayList;
-
-
-int rad = 60;        // Width of the shape
-float xpos, ypos;    // Starting position of shape    
-
-float xspeed = 2.8;  // Speed of the shape
-float yspeed = 2.2;  // Speed of the shape
-
-int xdirection = 1;  // Left or Right
-int ydirection = 1;  // Top to Bottom
 
 
 
@@ -47,9 +33,6 @@ void setup(){
   noStroke();
   frameRate(30);
   ellipseMode(RADIUS);
-  // Set the starting position of the shapes   https://hianime.to/watch/the-dungeon-of-black-company-15801?ep=81443&ep=81443
-  xpos = width/2;
-  ypos = height/2;
 }
 
 void draw() 
@@ -61,7 +44,6 @@ void draw()
     strokeWeight(8);
     
     line(-20, 404+topAdjust, 2000, 404+topAdjust);
-    
     noStroke();
     
     fill(69, 91, 255);
@@ -76,8 +58,9 @@ void draw()
     float pastLevelTOP = 0.0;
     float mpLevelBOT = 0.0;
     float pastLevelBOT = 0.0;
-                        
-  for(int i = riverLength; i > 100; i--){
+    
+    int step = 6;
+  for(int i = riverLength; i > 100; i-=step){
     for(int j = 0; j < mpArr.size(); j++){
       if (abs(mpArr.get(j).get(0) - i) < 12 && mpArr.get(j).get(1) < 400+topAdjust && mpArr.get(j).get(2) == 1.0){
         mpLevelTOP -=10*extraAdjust;
@@ -92,8 +75,8 @@ void draw()
     }
     //mpLevel = 100+((i)*(0.2));
     strokeWeight(1);
-    line(i+1, extraAdjust*(pastLevelTOP)+300+topAdjust, i, extraAdjust*(mpLevelTOP)+300+topAdjust);
-    line(i+1, extraAdjust*(pastLevelBOT)+700+adjust, i, extraAdjust*(mpLevelBOT)+700+adjust);
+    line(i+step, extraAdjust*(pastLevelTOP)+300+topAdjust, i, extraAdjust*(mpLevelTOP)+300+topAdjust);
+    line(i+step, extraAdjust*(pastLevelBOT)+700+adjust, i, extraAdjust*(mpLevelBOT)+700+adjust);
     
     if(i == 101){
       totalTopEnd += mpLevelTOP;
@@ -141,6 +124,30 @@ void draw()
 
   }/////////////////////////////////////////
   */
+  
+    noStroke();
+
+  fill(69, 91, 255);
+  rect(100, 700+adjust, riverLength, 100);
+  
+  
+
+    strokeWeight(2);
+
+  for(int i = 0; i < 9; i++){
+    stroke(255,0,0);
+  line(150+ (riverLength/10)*i, 300+topAdjust, 150+ (riverLength/10)*i, 400+topAdjust);
+  }
+  textSize(20);
+  text("Filtration location", 200, 200);
+  stroke(69, 91, 255);
+  line(250, 205, 290+5, 340+11);
+  line(290+5, 340+11, 270+5, 320+11);
+  line(290+5, 340+11, 298+5, 312+11);
+
+  
+  stroke(0,0,0);
+  
   for(int i = 0; i < mpArr.size(); i++){
       stroke(mpArr.get(i).get(3), mpArr.get(i).get(4), mpArr.get(i).get(5));
       strokeWeight(2);
@@ -149,52 +156,64 @@ void draw()
       }
         mpArr.get(i).set(0, mpArr.get(i).get(0) - 0.5); //-=0.5;
       
-      if( mpArr.get(i).get(0) % (riverLength/10) < 1 && random(0,1) < 1 && mpArr.get(i).get(1) < 400+topAdjust){
-         mpArr.get(i).set(2, 0.0);
+      if( mpArr.get(i).get(0) % (riverLength/10) < 1 && random(0,1) < 0.9054 && mpArr.get(i).get(1) < 400+topAdjust && time > 40){
+         //mpArr.get(i).set(2, 0.0);
+         mpArr.remove(i);
          }
+      if(mpArr.get(i).get(0) < 0){
+        mpArr.remove(i);
+      }
     
   }
   
-  for (int i = 0; i < 4; i++){
-    //float[] temp = {random(110, riverLength), random(310+topAdjust,390+topAdjust), true, random(0,255),random(0,255),random(0,255)};
-    ArrayList<Float> temp1 = new ArrayList<Float>();
-    temp1.add(random(110, riverLength));//x
-    temp1.add(random(310+topAdjust,390+topAdjust));//y
-    temp1.add(1.0);//exists
-    temp1.add(random(0,255));//r
-    temp1.add(random(0,255));//g
-    temp1.add(random(0,255));//b
-    mpArr.add(temp1);
+  if(mpArr.size() < 35000){
+    for (int i = 0; i < 4; i++){
+      //float[] temp = {random(110, riverLength), random(310+topAdjust,390+topAdjust), true, random(0,255),random(0,255),random(0,255)};
+      ArrayList<Float> temp1 = new ArrayList<Float>();
+      temp1.add(random(110, riverLength));//x
+      temp1.add(random(310+topAdjust,390+topAdjust));//y
+      temp1.add(1.0);//exists
+      temp1.add(random(0,255));//r
+      temp1.add(random(0,255));//g
+      temp1.add(random(0,255));//b
+      mpArr.add(temp1);
+      ArrayList<Float> temp2 = new ArrayList<Float>();
+      temp2.add(random(110, riverLength));//x
+      temp2.add(random(710+adjust,790+adjust));//y
+      temp2.add(1.0);//exists
+      temp2.add(random(0,255));//r
+      temp2.add(random(0,255));//g
+      temp2.add(random(0,255));//b
+      mpArr.add(temp2);
     
     //append(mpArr, [random(110, length), random(310+topAdjust,390+topAdjust), true, [random(0,255),random(0,255),random(0,255)]])
+    }
   }
-  
-  noStroke();
-
-  fill(69, 91, 255);
-  rect(100, 700+adjust, riverLength, 100);
-  
-  
-  strokeWeight(2);
-  stroke(0,0,0);
+  textSize(12);
+  text(extraAdjust, 50, 50);
+   text(-1*totalTopEnd/time, 150, 50);
+  text(-1*totalBotEnd/time, 150, 70);
+  text(mpArr.size(), 150, 90);
+  text(frameRate, 450, 70);
+  textSize(60);
+  fill(0);
+  text("River After Deployment", 940, 70);
+  text("River Before Deployment", 900, 550);
 
   //mpLevel = 0;
   
-  
-    
-  // Update the position of the shape
-  xpos = xpos + ( xspeed * xdirection );
-  ypos = ypos + ( yspeed * ydirection );
-  
-  // Test to see if the shape exceeds the boundaries of the screen
-  // If it does, reverse its direction by multiplying by -1
-  if (xpos > width-rad || xpos < rad) {
-    xdirection *= -1;
-  }
-  if (ypos > height-rad || ypos < rad) {
-    ydirection *= -1;
-  }
+    stroke(0);
+    strokeWeight(3);
+    fill(0);
+    line(100, -10, 100, height+10);
+    line(100-10, 300+topAdjust, 100+10, 300+topAdjust);
+    line(100-10, 700+adjust, 100+10, 700+adjust);
 
-  // Draw the shape
-  ellipse(xpos, ypos, rad, rad);
+    textSize(24);
+    rotate(-1*PI/2);
+    text("(Unitless) Particles / Second", -340, 90);
+    text("(Unitless) Particles / Second", -760, 90);
+
+    rotate(1*PI/2);
+    
 }
